7bde326944a3ce8906ea51ae64a20af63ae40ec4
Tiernan Martin <tiernan.martin@gmail.com>
2020-05-10 16:07:46 -0700

    add a few utility functions

---
+++ R/utils-pipe.R
@@ -0,0 +1,9 @@
+#' @title Pipe operator
+#' @description See \code{magrittr::\link[magrittr]{\%>\%}} for details. v
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
---
+++ R/utils.R
@@ -0,0 +1,8 @@
+#' @title A set of generic helpful functions
+#' @description General purpose functions to make working in R easier.
+#' @name utils
+#' @import purrr
+NULL
+
+#' @export
+g <- dplyr::glimpse
